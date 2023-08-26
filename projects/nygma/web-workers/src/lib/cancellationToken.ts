dd11c20f3cc5106ad6fd5dabe0b710a416ca03fb
Oleksii Shepel <alex.s@paymentop.com>
2023-08-26 12:22:49 +0300

    ongoing corrections

--- projects/nygma/web-workers/src/lib/cancellationToken.ts
+++ projects/nygma/web-workers/src/lib/cancellationToken.ts
@@ -1,34 +1,61 @@
 
 export class CancellationToken {
-  private shared: ArrayBuffer;
-  private array: Int32Array;
+  static MAX_NUMBER_OF_WORKERS = 128;
 
-  constructor() {
+  private static booked: boolean[] = new Array<boolean>(this.MAX_NUMBER_OF_WORKERS);
+  private static shared: ArrayBuffer = crossOriginIsolated? new SharedArrayBuffer(Int32Array.BYTES_PER_ELEMENT * this.MAX_NUMBER_OF_WORKERS): new ArrayBuffer(0);
+  private static array: Int32Array = new Int32Array(this.shared);
+
+  private tokenIndex: number;
+
+  private constructor(offset: number) {
     if(!crossOriginIsolated) {
       console.warn("CancellationToken is not supported in this environment. Please add following two headers to the top level document: 'Cross-Origin-Embedder-Policy': 'require-corp'; 'Cross-Origin-Opener-Policy': 'same-origin';");
     }
 
-    this.shared = crossOriginIsolated? new SharedArrayBuffer(4): new ArrayBuffer(4);
-    this.array = new Int32Array(this.shared);
+    this.tokenIndex = offset;
   }
 
-  public cancel(): void {
-    if (this.array) {
-      Atomics.store(this.array, 0, 1);
+  static register(): CancellationToken {
+    const index = this.booked.findIndex(item => !item);
+    if(index === -1) {
+      throw new Error('Number of cancellation tokens exceeded the admissible limit');
+    } else if(CancellationToken.withinArray(index)) {
+      this.booked[index] = true;
+      Atomics.store(CancellationToken.array, index, 0);
+    }
+    return new CancellationToken(index);
+  }
+
+  free() {
+    CancellationToken.booked[this.tokenIndex] = false;
+  }
+
+  cancel(): void {
+    if (CancellationToken.array && CancellationToken.withinArray(this.tokenIndex)) {
+      Atomics.store(CancellationToken.array, this.tokenIndex, 1);
     }
   }
 
-  public reset(): void {
-    if (this.array) {
-      Atomics.store(this.array, 0, 0);
+  reset(): void {
+    if (CancellationToken.array && CancellationToken.withinArray(this.tokenIndex)) {
+      Atomics.store(CancellationToken.array, this.tokenIndex, 0);
     }
   }
 
-  public get cancelled(): boolean {
-    return !!this.array && this.array[0] === 1;
+  get cancelled(): boolean {
+    return CancellationToken.withinArray(this.tokenIndex) && Atomics.load(CancellationToken.array, this.tokenIndex) === 1;
   }
 
-  public get buffer(): ArrayBuffer {
-    return this.shared;
+  get index(): number {
+    return this.tokenIndex
+  }
+
+  static get buffer(): ArrayBuffer {
+    return CancellationToken.shared;
+  }
+
+  private static withinArray(index: number): boolean {
+    return index > -1 && CancellationToken.array.byteLength / 4 > index;
   }
 }
